8a890ff0caf86021869e534b76635a61977f30d9
franco felippelli <legolegofran@hotmail.com>
2019-08-05 23:00:25 -0300

    opciones

--- app/src/main/java/aplicacion/android/feli/httpprueba/MainActivity.java
+++ app/src/main/java/aplicacion/android/feli/httpprueba/MainActivity.java
@@ -1,5 +1,6 @@
 package aplicacion.android.feli.httpprueba;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
